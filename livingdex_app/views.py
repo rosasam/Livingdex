6f6bb67564b839c9d5c4a6a59ba1748a99824c09
Samuel Rosas <samuel.rosas@aalto.fi>
2017-03-04 11:49:42 +0200

    Create block layout

--- livingdex_app/views.py
+++ livingdex_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseForbidden
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.conf import settings
+from livingdex_app.models import *
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the livingdex start page.")
